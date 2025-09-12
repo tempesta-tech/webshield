ea31ea14975978b370ed54720143ab29b537faa0
symstu <stukalo.maksym@gmail.com>
2025-09-12 15:49:00 +0200

    migrate test from unittest to pytest

--- tests/test_ja5_config.py
+++ tests/test_ja5_config.py
@@ -1,5 +1,5 @@
 import os
-import unittest
+import pytest
 
 from utils.ja5_config import Ja5Config, Ja5Hash
 
@@ -8,68 +8,71 @@ __copyright__ = "Copyright (C) 2023-2025 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
 
-class TestJa5Config(unittest.TestCase):
-    def setUp(self):
-        self.path_to_config = "/tmp/tmp-hashes"
+@pytest.fixture
+def config_file(self):
+    path_to_config = "/tmp/tmp-hashes"
 
-        with open(self.path_to_config, "w") as f:
-            f.write(
-                "hash aaaaaaa11111 3 4;\n"
-                "  2222aaaaaaa 12   23444  ;  \n"
-                " hash wrong222 12   ;  \n"
-                "  hash wrong-again  ;  \n"
-                "#commented  ;  \n"
-            )
+    with open(path_to_config, "w") as f:
+        f.write(
+            "hash aaaaaaa11111 3 4;\n"
+            "  2222aaaaaaa 12   23444  ;  \n"
+            " hash wrong222 12   ;  \n"
+            "  hash wrong-again  ;  \n"
+            "#commented  ;  \n"
+        )
+    yield path_to_config
+    os.remove(path_to_config)
 
-    def tearDown(self):
-        os.remove(self.path_to_config)
 
-    def test_config_does_not_exists(self):
-        with self.assertRaises(FileNotFoundError):
-            config = Ja5Config("/tmp/non-existing.conf")
-            config.verify_file()
+def test_config_does_not_exists():
+    with pytest.raises(FileNotFoundError):
+        config = Ja5Config("/tmp/non-existing.conf")
+        config.verify_file()
 
-    def test_load_hashes_from_file(self):
-        config = Ja5Config(self.path_to_config)
-        config.load()
 
-        self.assertEqual(len(config.hashes), 1)
+def test_load_hashes_from_file(config_path):
+    config = Ja5Config(config_path)
+    config.load()
 
-    def test_dump_file(self):
-        config = Ja5Config(self.path_to_config)
-        config.load()
+    assert len(config.hashes) == 1
 
-        config.hashes = {"test": Ja5Hash(value="0", connections=1, packets=1)}
-        config.dump()
 
-        with open(self.path_to_config) as f:
-            data = f.read()
+def test_dump_file(config_path):
+    config = Ja5Config(config_path)
+    config.load()
 
-        self.assertEqual(data, "hash 0 1 1;\n")
+    config.hashes = {"test": Ja5Hash(value="0", connections=1, packets=1)}
+    config.dump()
 
-    def test_modification(self):
-        config = Ja5Config(self.path_to_config)
-        config.load()
-        self.assertEqual(config.need_dump, False)
+    with open(config_path) as f:
+        data = f.read()
 
-        config.add(Ja5Hash(value="100", connections=1, packets=2))
-        self.assertEqual(config.need_dump, True)
+    assert data == "hash 0 1 1;\n"
 
-        config.dump()
-        self.assertEqual(config.need_dump, False)
 
-        with open(self.path_to_config) as f:
-            data = f.read()
+def test_modification(config_path):
+    config = Ja5Config(config_path)
+    config.load()
+    assert config.need_dump is False
 
-        self.assertEqual(data, "hash aaaaaaa11111 3 4;\nhash 100 1 2;\n")
+    config.add(Ja5Hash(value="100", connections=1, packets=2))
+    assert config.need_dump == True
 
-        config.remove("100")
-        self.assertEqual(config.need_dump, True)
+    config.dump()
+    assert config.need_dump == False
 
-        config.dump()
-        self.assertEqual(config.need_dump, False)
+    with open(config_path) as f:
+        data = f.read()
 
-        with open(self.path_to_config) as f:
-            data = f.read()
+    assert data == "hash aaaaaaa11111 3 4;\nhash 100 1 2;\n"
 
-        self.assertEqual(data, "hash aaaaaaa11111 3 4;\n")
+    config.remove("100")
+    assert config.need_dump is True
+
+    config.dump()
+    assert config.need_dump == False
+
+    with open(config_path) as f:
+        data = f.read()
+
+    assert data == "hash aaaaaaa11111 3 4;\n"
